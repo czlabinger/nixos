c41d82c4c7547ec790f84e99550f8a8b90f01bde
czlabinger <czlabinger@student.tgm.ac.at>
2024-03-14 10:42:40 +0100

    Updated Readme

--- hosts/WKS012-NixOS/configuration.nix
+++ hosts/WKS012-NixOS/configuration.nix
@@ -199,7 +199,6 @@
     cargo
     rustup
     rust-analyzer
-    candy-icons
     yarn
   ];
 
@@ -251,13 +250,7 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
+  system.stateVersion = "23.11"; # Don't edit!
 
   programs.zsh.enable = true;
   programs.hyprland.enable = true;
--- hosts/WKS012-NixOS/home.nix
+++ hosts/WKS012-NixOS/home.nix
@@ -14,19 +14,10 @@
     ../../modules/home-manager/neovim
   ];
 
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "stoffi05";
   home.homeDirectory = "/home/stoffi05";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "23.11"; # Please read the comment before changing.
+  home.stateVersion = "23.11"; # Don't edit!
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
@@ -69,35 +60,10 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
     ".jdk".source = pkgs.jdk21;
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. If you don't want to manage your shell through Home
-  # Manager then you have to manually source 'hm-session-vars.sh' located at
-  # either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/stoffi05/etc/profile.d/hm-session-vars.sh
-  #
+  # 'home.sessionVariables'. # Change env variables
 
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
