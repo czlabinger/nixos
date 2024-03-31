39beea0a39fbfddb019898675818c59f54e25667
czlabinger <czlabinger@student.tgm.ac.at>
2024-03-31 09:40:21 +0200

    66 current  2024-03-31 09:40:15  24.05.20240325.57e6b3a       6.6.22                          *

--- hosts/WKS012-NixOS/configuration.nix
+++ hosts/WKS012-NixOS/configuration.nix
@@ -20,8 +20,13 @@
     };
   };
 
+  nix.settings = {
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+    auto-optimise-store = true;
+  };
+
   # Delete older than 7d
-  nix.settings.auto-optimise-store = true;
   nix.gc.automatic = true;
   nix.gc.dates = "daily";
   nix.gc.options = "--delete-older-than 7d";
