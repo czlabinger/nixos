27ec14d5196139f4f264e98a4d51a17b4220f0d6
czlabinger <czlabinger@student.tgm.ac.at>
2024-04-08 16:12:25 +0200

    95 current  2024-04-08 16:12:19  24.05.20240406.ff0dbd9  6.6.25                          *

--- hosts/WKS012-NixOS/configuration.nix
+++ hosts/WKS012-NixOS/configuration.nix
@@ -62,7 +62,7 @@
     enable = true;
     theme = "${import ../../modules/nixos/sddm/tokyo-night.nix {inherit pkgs;}}";
   };
-  #services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
