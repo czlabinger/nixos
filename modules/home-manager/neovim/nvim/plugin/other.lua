1d8f28860f494586d919f63cc856181fb30f4bca
czlabinger <czlabinger@student.tgm.ac.at>
2024-04-13 14:37:07 +0200

    112 current  2024-04-13 14:33:55  24.05.20240408.4cba8b5  6.6.25                          *

--- modules/home-manager/neovim/nvim/plugin/other.lua
+++ modules/home-manager/neovim/nvim/plugin/other.lua
@@ -3,3 +3,11 @@ vim.cmd("colorscheme gruvbox")
 
 -- Comment
 require("Comment").setup()
+
+-- venv
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = {"python"},
+    callback = function()
+        require('swenv.api').auto_venv()
+    end
+})
