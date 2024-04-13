90a510db6195296d407fa7fef113e0c7f4c57481
czlabinger <czlabinger@student.tgm.ac.at>
2024-04-13 14:52:45 +0200

    114 current  2024-04-13 14:43:14  24.05.20240408.4cba8b5  6.6.25                          *

--- modules/home-manager/neovim/nvim/plugin/other.lua
+++ modules/home-manager/neovim/nvim/plugin/other.lua
@@ -6,10 +6,3 @@ require("Comment").setup()
 
 -- venv
 require("swenv").setup()
-
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = {"python"},
-    callback = function()
-        require('swenv.api').auto_venv()
-    end
-})
