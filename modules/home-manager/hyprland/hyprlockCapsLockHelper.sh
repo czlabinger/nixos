0e61b4fb466530f4e98c3dea79b4f98fb8f0cdf7
czlabinger <czlabinger@student.tgm.ac.at>
2024-11-18 13:46:33 +0100

    HyprlockCapslockHelper

--- modules/home-manager/hyprland/hyprlockCapsLockHelper.sh
+++ modules/home-manager/hyprland/hyprlockCapsLockHelper.sh
@@ -1,5 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-hyprctl = $(hyprctl devices -j)
+capsState=$(hyprctl devices -j | jq -r '.keyboards[-1].capsLock')
+out=""
 
+if [[ "$capsState" == "true" ]]; then
+    out="CapsLock: On"
+else
+    out="CapsLock: Off"
+fi
 
+echo "$out"
+exit
